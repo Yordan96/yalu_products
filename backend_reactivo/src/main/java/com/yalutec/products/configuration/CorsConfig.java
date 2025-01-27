1e56cc6a75bd8d488f58fa9f68a8623214797978
yordan <yordan@oxperience.com>
2025-01-27 16:52:16 -0600

    Se agrego configuracion de cors para aceptar al frontend

---
+++ backend_reactivo/src/main/java/com/yalutec/products/configuration/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.yalutec.products.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+@Configuration
+@EnableWebFlux
+public class CorsConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica CORS a todas las rutas
+                .allowedOrigins("http://localhost:4200") // Permitir solicitudes del frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los encabezados
+                .allowCredentials(true); // Permitir credenciales
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
